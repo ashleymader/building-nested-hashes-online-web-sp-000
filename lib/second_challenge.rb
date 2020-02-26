9f02ff5f9f1e5c6a3e564df5b2ea3d4377e6708f
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-26 14:44:49 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,6 +3,6 @@ def second_challenge
     :montague => {:patriarch => {}, :matriarch => {},:hero => {}, :hero_friends=> []
       }, 
     :capulet => { :patriarch => {}, :matriarch => {},:heroine => {},:heroine_friends => []
-      }}
+      }
 
 end
