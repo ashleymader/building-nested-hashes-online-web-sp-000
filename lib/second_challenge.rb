949a62bbc1a2067cb5a75006d6189fe2010b1e26
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-26 14:42:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {:patriarch, :matriarch, :hero, :hero_friends
       }, 
-    :capulet => { :patriarch, :matriarch
+    :capulet => { :patriarch, :matriarch, :heroine
       #your key/value pairs here!
       }}
 
